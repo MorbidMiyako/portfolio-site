e22c13de1ab4aa32225bcc68b3fe7f2d211a256c
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-07-25 14:41:44 +0100

    adds jest, test to check if jest works, general setup for tests in notes.txt

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("check if jest works", () => {
+  test("tests work", () => {
+    expect(1 + 1).not.toBe(3)
+  })
+})
