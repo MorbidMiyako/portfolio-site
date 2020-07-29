f22b551e930beeaed47151c5133443fb4d4827c8
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2020-07-29 22:19:44 +0100

    adds flickering stars to the background

--- src/helpers/CreateStars.js
+++ src/helpers/CreateStars.js
@@ -2,7 +2,14 @@ export default function CreateStars(numOfStars) {
     const stars = []
     for (let i = 0; i < numOfStars; i++) {
         const newStar = {
-            pxSize: Math.random()
+            size: 25 * Math.random() ^ 4 + 5,
+            top: 100 * Math.random(),
+            left: 100 * Math.random(),
+            // size: 20,
+            // top: 0,
+            // left: 0,
+            duration: 55 * Math.random() + 5,
+            delay: 0
         }
         stars.push(newStar)
     }
